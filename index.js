8869da2240dc32450a16eeab9d9675076578d18f
Ijem <ij.ofili@gmail.com>
2015-01-21 11:40:36 +0000

    added index.html file

---
+++ index.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html class="">
+
+<head>
+    <title>playplay</title>
+    <link rel="stylesheet" href="">
+</head>
+<body>
+
+
+    <script src="scripts/main.js"></script>
+</body>
+</html>
